4332fa7e8e799d0f50fee4c512f8c8b5f4c9da1b
Cade Gillem <cpgillem@gmail.com>
2014-11-18 13:47:33 -0500

    Deprecating the files module.

--- files.py
+++ files.py
@@ -1,57 +1,59 @@
 import io
 
-# Holds a two dimensional array of data to be read or written from a file.
-# The file is read in the constructor. If overwrite is set to True, the file is not read.
-# The file is written with the write() function. Use the write() function when finished with the file.
+# A file that can hold key-value pairs, or just plain values.
 class DataFile:
-    def __init__(self, filename, overwrite = False, line_delimiter = ';', column_delimiter = ','):
+    def __init__(self, filename, overwrite = False, entry_separator = ';', key_value_separator = ':'):
         self.filename = filename
         self.overwrite = overwrite
-        self.line_delimiter = line_delimiter
-        self.column_delimiter = column_delimiter
-        self.data = []
+        self.entry_separator = entry_separator
+        self.key_value_separator = key_value_separator
+        self.data = {}
 
         # If the file is not set to overwrite, import all the contents of the file.
         if not overwrite:
             # Open the file to read.
             with open(filename, 'r') as f:
+                # Read the file as a string
                 string = ""
-                # For each line in the file:
                 for line in f:
-                    # Add the line to the raw string.
                     string += line
-            # Slice up the string and put it into this object's data field.
-            lines = tokenize(string, line_delimiter)
-            for line in lines:
-                values = tokenize(line, column_delimiter)
-                self.data.append(values)
+                entries = tokenize(string, entry_separator)
 
-    # Write all current data to the file.
+                # Add each key-value pair to the dictionary.
+                for entry in entries:
+                    key_value = split_by(string, key_value_separator)
+                    if len(key_value) > 1:
+                        self.data[key_value[0]] = key_value[1]
+                    # If there wasn't a key value, don't add anything.
+
+    # Writes all current data to the file.
     def write(self):
-        string = ""
-        for i in range(0, len(self.data)):
-            for j in range(0, len(self.data[i])):
-                string += self.data[i][j]
-                if j < len(self.data[i]) - 1:
-                    string += self.column_delimiter
-            if i < len(self.data) - 1:
-                string += self.line_delimiter
         with open(self.filename, 'w') as f:
-            f.write(string)
+            for key, value in self.data:
+                f.write(key)
+                f.write(key_value_separator)
+                f.write(value)
+                f.write(entry_separator)
 
-# Split a string by a character, but ignore it if it is preceded by the escape character.
+# Splits a string by a delimiter once. Returns an array of two strings. If the delimiter is not found, returns an array of the original string.
+def split_by(string, delimiter):
+    delim_location = string.find(delimiter)
+    if delim_location >= 0:
+        return [string[:delim_location], string[delim_location + 1:]]
+    else:
+        return [string]
+
+# Splits a string by a character, but ignores it if it is preceded by the escape character.
 def tokenize(string, delimiter = ',', escape = '\\'):
     array = []
     buf = ""
     escaping = False
     for c in string:
-        if c == delimiter:
-            if not escaping:
-                array.append(buf)
-                buf = ""
-            else:
-                escaping = False
-        elif c == escape:
+        if c == delimiter and not escaping:
+            array.append(buf)
+            print(buf)
+            buf = ""
+        elif c == escape and not escaping:
             escaping = True
         else:
             buf += c

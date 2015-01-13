e198ba85390547ca0a81c261cd2378039b9439a0
mobyte <rob@wireless144-113.wireless.wmich.edu>
2015-01-13 16:08:59 -0500

    Started working on tweet command

--- commands/tweet.py
+++ commands/tweet.py
@@ -1,9 +1,6 @@
 @command("tweet")
 def tweet(nick, channel, message):
-    CONSUMER_KEY = "jpAHE5alGs9jx44dFZwXeq1Qh"
-    CONSUMER_SECRET = "occSPW1TPd5yZxr9EfB4r1lsCADk6wqQaEmdRJLgJeRjxG5jYy"
-    ACCESS_KEY = "172595184-zE9JlBmyoaULNrNBeNpvBSINLKcfYd7EG1lD7fAx"
-    ACCESS_SECRET = "gO2BastCjP0W5MjVDSCebwjNVMjiwyGRNkH0SvT0AVgvk"
+    import commands.api
     apiurl = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" +  message.split()[0] +"&api_key=" + apikey + "&format=json"
         jsonread = urllib.request.urlopen(apiurl)
         jsondata = jsonread.read()

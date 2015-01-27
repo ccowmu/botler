cd5b7ece59fa9b10371694c08d91192e5aee3fba
Benjamin Campbell <twitch043@gmail.com>
2015-01-27 08:34:44 -0500

    Update choose.py

--- commands/choose.py
+++ commands/choose.py
@@ -3,8 +3,10 @@ import random
 @command("choose")
 def choose(nick, channel, message):
     choices = message.split(sep = ",")
+    #this line rids of empty choices caused by consecutive commas
     choices = [i for i in choices if i != '']
     choice = random.sample(choices, 1)
+    #this line cleans up the winner, removing leading spaces and brackets
     winner = "".join(choice).strip()
 
     say(channel, "{}: {}".format(nick, winner))

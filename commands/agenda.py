c9ba06478a40f0b9c9a591bc77e4fe0ca1f523c6
James Jenkins <james.p.jenkins@wmich.edu>
2015-02-11 13:34:35 -0500

    Removed agenda command from this branch.

--- commands/agenda.py
+++
@@ -1,5 +0,0 @@
-white_list=("themind","flay","stringy","sphinx")
-@command("agenda")
-def agenda(nick, channel, message):
-    if nick not in white_list:
-        say("%s: Sorry, you're not authorized to use this feature."%nick)

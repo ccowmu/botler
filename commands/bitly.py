e89cbc15fb82e72615bd124099918188b31a2504
mobyte <rob@wireless152-064.wireless.wmich.edu>
2015-02-13 21:36:21 -0500

    Fixed errors with bugged commands + topic command.

--- commands/bitly.py
+++ commands/bitly.py
@@ -1,19 +1,23 @@
 @command("bitly")
 def bitly(nick, channel, message):
     import commands.api
+    import requests
+    from urllib.parse import quote
     import json
-    import urllib.request
-    longurl = message
     try:
-        bitlyurl = "https://api-ssl.bitly.com/v3/shorten?access_token=" + bitlykey + "&longUrl=" + longurl + "/"
-        jsonread = urllib.request.urlopen(message)
-        jsondata = jsonread.read()
-        api = json.loads(jsondata.decode('utf8'))
-        shorturl = api['data']['url']
+        link = message.split()[0]
+        if(link.startswith("http://") == False):
+            link = "http://" + link
+        params = {'longUrl': link, 'format': 'json', 'access_token': bitlykey}
+        r = requests.get('https://api-ssl.bitly.com/v3/shorten', params=params)
+        data = json.loads(r.content.decode('utf8'))
+        shorturl = data['data']['url']
         say(channel, '{}: {}'.format(nick, shorturl))
     except ValueError:
         say(channel, '{}: Invalid URL given.'.format(nick))
     except TypeError:
+        say(channel, '{}: Invalid URL given.'.format(nick))
+    except IndexError:
         say(channel, '{}: Invalid URL given.'.format(nick))
     else:
         pass

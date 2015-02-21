d711ff4c2e7d34fcfce0acf3fc6e069976c741d7
scott-linder <scott.b.linder@wmich.edu>
2015-02-21 18:54:23 -0500

    Fix formatting and string interpolation in query

--- commands/seen.py
+++ commands/seen.py
@@ -4,10 +4,12 @@
 def seen(nick,user,channel,message):
 	with db as conn:	
 		with conn.cursor() as cursor:
-			cursor.execute("SELECT * from log where nick = '%s' order by time desc limit 1;" % message)
-			msg = str(cursor.fetchone())
-			msg = msg.split(',')
-			print(msg)
-			say(channel, ("%s was last seen on %s saying %s in %s" % (message.lstrip('!seen '), 'FIX TIME', msg[9], msg[10])))
+			cursor.execute("SELECT time, nick, message, channel from log where nick = %s order by time desc limit 1;", (message,))
+			row = cursor.fetchone()
+			time = row[0]
+			nick = row[1]
+			msg = row[2]
+			chan = row[3]
+			say(channel, '{} was last seen on {} saying "{}" in {}'.format(nick, time, msg, chan))
 
 # 3 (minutes ago) on 4, 1, 2 ,0 #

b975026885305d8ef22fcb7a86bda3f5efe7e32f
flayisflay <flayisflay@gmail.com>
2015-02-21 18:23:57 -0500

    STRINGY, COME FIND ME

--- commands/seen.py
+++ commands/seen.py
@@ -10,4 +10,4 @@ def seen(nick,user,channel,message):
 			print(msg)
 			say(channel, ("%s was last seen on %s saying %s in %s" % (message.lstrip('!seen '), 'FIX TIME', msg[9], msg[10])))
 
-# 3 (minutes ago) on 4, 1, 2 ,0
+# 3 (minutes ago) on 4, 1, 2 ,0 #

645006807b7024a95127a92e1dfd2a92fc00d191
flayisflay <flayisflay@gmail.com>
2015-02-20 19:27:32 -0500

    Seen mod

--- commands/seen.py
+++ commands/seen.py
@@ -2,9 +2,12 @@
 @command("seen")
 
 def seen(nick,user,channel,message):
-	print(db)	
-	query = "SELECT * from log where nick = 'flay' order by time desc limit 1;"
-	say(channel, '{}:'.format(nick) + "Searching for %s"  % nick)
-	db.cusor().execute(query)
+	if db == None:
+		return print("working") 
+		query = "SELECT * from log where nick = 'flay' order by time desc limit 1;"
+		print(channel, '{}:'.format(nick) + "Searching for 'stringy'")
+		return print(db.cusor().execute(query))
+		print('hi')
+
 	
 

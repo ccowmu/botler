045774d5a64cd1aaaa742df440770d592b85bb9e
flayisflay <flayisflay@gmail.com>
2015-02-20 22:26:00 -0500

    Update 'n share

--- commands/seen.py
+++ commands/seen.py
@@ -2,12 +2,9 @@
 @command("seen")
 
 def seen(nick,user,channel,message):
-	if db == None:
-		return print("working") 
-		query = "SELECT * from log where nick = 'flay' order by time desc limit 1;"
-		print(channel, '{}:'.format(nick) + "Searching for 'stringy'")
-		return print(db.cusor().execute(query))
-		print('hi')
+	with db as conn:	
+		with conn.cursor() as cursor:
+			return cursor.execute("SELECT * from log where nick = 'flay' order by time desc limit 1;")
 
 	
 

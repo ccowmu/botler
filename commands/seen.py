e56c1643b13142796fa84a1c22f392670975d18f
scott-linder <scott.b.linder@wmich.edu>
2015-02-21 19:13:48 -0500

    Handle case where no row is found

--- commands/seen.py
+++ commands/seen.py
@@ -1,4 +1,4 @@
-#Flays seen function
+#*Flays seen function
 @command("seen")
 
 def seen(nick,user,channel,message):
@@ -6,10 +6,13 @@ def seen(nick,user,channel,message):
 		with conn.cursor() as cursor:
 			cursor.execute("SELECT time, nick, message, channel from log where nick = %s order by time desc limit 1;", (message,))
 			row = cursor.fetchone()
-			time = row[0]
-			nick = row[1]
-			msg = row[2]
-			chan = row[3]
-			say(channel, '{} was last seen on {} saying "{}" in {}'.format(nick, time, msg, chan))
+			if row == None:
+				say(channel, "No record of {}".format(message))
+			else:
+				time = row[0]
+				nick = row[1]
+				msg = row[2]
+				chan = row[3]
+				say(channel, '{} was last seen on {} saying "{}" in {}'.format(nick, time, msg, chan))
 
 # 3 (minutes ago) on 4, 1, 2 ,0 #

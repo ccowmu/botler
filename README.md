5c3cd967645f925dce8cc39c70280000a26bbbbc
flayisflay <flayisflay@gmail.com>
2015-09-26 23:49:51 -0400

    Included datetiming in filename generation. Other minor changes

--- README.md
+++ README.md
@@ -16,5 +16,17 @@ On yakko this might look something like:
     $ pip install -r requirements.txt
 
 Every time you want to work on botler from a new terminal, you will need to run:
-
     $ . env/bin/activate
+Also for the db, you're going to need to export the DB_PASS variable so that botler can run.
+    $ export DB_PASS=$ASKSOMEONEFORTHEPASS
+
+#Usage Advice
+If your /command is throwing errors then try and !reload ; The status window will give you error locations; highly useful for debugging.
+
+Make sure that your @command is (aside from #comment_blocks) the very first line in the file, or the command will not load properly.
+
+Its recommended to use 4 spaces, instead of tabs. Set this in .vimrc to with the following;
+
+    $ tabstop=4
+    $ shiftwidth=4
+    $ set expandtab
--- commands/query.py
+++ commands/query.py
@@ -1,11 +1,12 @@
-#*Flays sql query function
-@command("query")
+#*Flays arbitrary sql query function
+import datetime
+@command("query", man= "Usage is !query SQL QUERY GOES HERE")
 def query(nick,user,channel,message):
     try:
         os.mkdir("results/")
     except FileExistsError:
         pass
-    with open('results/results.txt', mode = 'w') as resultsfile:
+    with open('../www/SQL_queries/results: '+ str(user) + datetime.datetime.now().strftime("-%Y-%m-%d-%S"), mode ='a+', encoding='utf-8') as resultsfile:
         if db == None:
             return
         with db as conn:
@@ -15,6 +16,11 @@ def query(nick,user,channel,message):
                     if result == None:
                         say(channel, "ERROR FROM : {}".format(message))
                     else:
-                        #resultsfile.write(str(result))
-                        say(channel, "Results written to file")
-                        #fix with gist shit from m00se
+                        #resultsfile.write(str(message)
+                        for row in result:
+                            resultsfile.write(str(row)+ '\n')
+                        say(channel, "Results written to file : ~/flay/SQL_queries/result.txt")
+                        resultsfile.close()
+                            #fix with gist shit from m00se
+                        #Consider an aliasing function, to allow users to create aliased queries, sparing themselves from rewriting the entire query every time.
+# Create some sugar to pull out 
